fb502bce7dfdbabfdcae708e2a4e7aa5c32ee111
Ranjit Solomon <ranjitsolo@gmail.com>
2022-10-24 01:29:23 -0400

    Added loops, if-else, case-switch

--- first_script.sh
+++ first_script.sh
@@ -5,6 +5,8 @@ if [[ $nme == "" ]]; then
 	echo "What is your name?"
 	read name
 	echo "Good Morning $name"
+elif [[ $nme == "ranjit" ]]; then
+	echo "Good Morning Ranjit"
 else
 	echo "Good Morning $nme"
 fi
@@ -50,7 +52,34 @@ do
 	echo $val
 done
 
+echo "Please select your favorite fruit:
+1 - Apple
+2 - Banana
+3 - Orange"
 
+read fruit
+
+case $fruit in 
+	1)
+		echo "Apple was selected"
+		;;
+	2)
+		echo "Banana was selected"
+		;;
+	3)
+		echo "Orange was selected"
+		;;
+
+	*)
+		echo "Selected some unknown fruit"
+		;;
+esac
+
+
+
+
+# OR  ||
+# AND &&
 
 
 
